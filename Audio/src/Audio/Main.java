f9d090f2302716133cbbc33a24e40bcbba0b46cd
Manas Uniyal <manasuniyal100@gmail.com>
2019-08-29 00:25:30 +0530

    Feat: Update AudioUtility

--- Audio/src/Audio/Main.java
+++ Audio/src/Audio/Main.java
@@ -1,8 +1,48 @@
-package Audio;
+package sample;
 
-public class Main {
+//For accessing this utility, keep a static media player named mediaPlayer
 
-    public static void main(String[] args) {
-	// write your code here
+import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+import java.nio.file.Paths;
+
+public class AudioUtility extends Application {
+
+    public static MediaPlayer mediaPlayer;
+
+    public void play(String path){
+        String uri=Paths.get(path).toUri().toString();
+        Media media = new Media(uri);
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+        mediaPlayer.play();
     }
+
+    public void play(){
+        if(mediaPlayer.getStatus() == MediaPlayer.Status.PAUSED){
+            mediaPlayer.play();
+        }
+    }
+
+    public void pause(){
+        if(mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING){
+            mediaPlayer.pause();
+        }
+    }
+
+    public void setVolume(double vol){
+        mediaPlayer.setVolume((float)vol/100);
+    }
+
+    public void mute(){
+        mediaPlayer.setVolume(0.0);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+
 }
